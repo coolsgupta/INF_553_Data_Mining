5f4efd8042a5b9c368fd5883bde77621b0b2b410
Sachin Gupta <cool.sgupta@gmail.com>
2020-06-01 16:27:21 -0700

    renamde variables

--- Assignment 1/task3.py
+++ Assignment 1/task3.py
@@ -17,20 +17,20 @@ if __name__ == '__main__':
     results = dict()
 
     sc = SparkContext('local[8]', 'task2')
-    reviews_json = sc.textFile(argv[1]).map(lambda x: json.loads(x)).map(lambda x: (x['business_id'], 1))
+    reviews_count_tuples = sc.textFile(argv[1]).map(lambda x: json.loads(x)).map(lambda x: (x['business_id'], 1))
 
     if argv[3] == 'default':
-        reviews_data = reviews_json
+        business_review_map = reviews_count_tuples
         # business_review_map = reviews_data.map(lambda x: (x['business_id'], 1))
 
     else:
-        reviews_data = reviews_json.partitionBy(argv[4], hash_partition_business_id)
+        business_review_map = reviews_count_tuples.partitionBy(argv[4], hash_partition_business_id)
 
     # business_review_map = reviews_data.map(lambda x: (x[0], 1))
-    business_review_count = reviews_data.reduceByKey(lambda a, b: a + b)
+    business_review_count = business_review_map.reduceByKey(lambda a, b: a + b)
 
-    results['n_partitions'] = reviews_data.getNumPartitions()
-    results['n_items'] = reviews_data.glom().map(len).collect()
+    results['n_partitions'] = business_review_map.getNumPartitions()
+    results['n_items'] = business_review_map.glom().map(len).collect()
     results['result'] = business_review_count.filter(lambda x: x[1] > argv[5]).map(lambda x: list(x)).collect()
 
     with open(argv[2], 'w') as result_file:
